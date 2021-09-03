00bbbb2dc8f62be4f67cbc16e35a240a0d30d816
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-03 12:20:52 -0300

    Adicionando e dado funcionalidade ao  Menu, possibilitando cadastro de varios funcionarios

--- src/br/com/zup/Desafio1.java
+++ src/br/com/zup/Desafio1.java
@@ -9,7 +9,7 @@ public class Desafio1 {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
-        Map<String,String> listaFuncionarios = new HashMap<String,String>();
+        Map<String, String> listaFuncionarios = new HashMap<String, String>();
 
 
         //variáveis
@@ -17,26 +17,66 @@ public class Desafio1 {
         String emailFuncionario;
         String nomeFuncionario;
         String telefoneFuncionario;
+        int opcaoMenuSelecionada = 0;
+        boolean exibirMenu = true;
 
-        //recebendo dados do usuário
-        System.out.println("------Cadastro Funcionarios Zup------");
-        System.out.println("Digite o nome completo do funcionário: ");
-        nomeFuncionario = leitor.nextLine();
-        System.out.println("Digite o CPF do funcionário");
-        cpfFuncionario = leitor.nextLine();
-        System.out.println("Digite o e-mail do funcionário:");
-        emailFuncionario = leitor.nextLine();
-        System.out.println("Digite o telefone do funcionário: ");
-        telefoneFuncionario = leitor.nextLine();
 
-        //adicionando funcionário a lista
-        listaFuncionarios.put(cpfFuncionario, "|| Nome: " + nomeFuncionario + " || Telefone: " + telefoneFuncionario + " || Email: " + emailFuncionario);
+        //Menu
+        while (exibirMenu) {
+            System.out.println("------ Zup Innovation ------");
+            System.out.println("O que você deseja fazer?");
+            System.out.println("1- Cadastrar novo funcionário.");
+            System.out.println("2- Exibir lista de todos os funcionários cadastrados.");
+            System.out.println("3- Sair!");
 
-        //imprimindo lista
-        for (String impressaLista: listaFuncionarios.keySet()) {
-            System.out.println("------Zuppers Cadastrados------");
-            System.out.println("Funcionario: " + listaFuncionarios.get(impressaLista) + " || CPF: " + impressaLista);
+            //recebendo oção do usuário
+            opcaoMenuSelecionada = leitor.nextInt();
+            leitor.nextLine(); //adicionar o enter do usuário para não dar erro!
+
+            //Opções Menu
+            if (opcaoMenuSelecionada == 1) {
+
+                System.out.println("------Cadastro Funcionarios Zup------");
+
+                //Recebendo dados do usuário para cadastro
+                System.out.println("Digite o nome completo do funcionário: ");
+                nomeFuncionario = leitor.nextLine();
+                System.out.println("Digite o CPF do funcionário");
+                cpfFuncionario = leitor.nextLine();
+                System.out.println("Digite o e-mail do funcionário:");
+                emailFuncionario = leitor.nextLine();
+                System.out.println("Digite o telefone do funcionário: ");
+                telefoneFuncionario = leitor.nextLine();
+
+                //adicionando funcionário a lista
+                listaFuncionarios.put(cpfFuncionario, nomeFuncionario + " ||Telefone: " + telefoneFuncionario + " ||Email: " + emailFuncionario);
+
+                System.out.println("Funcionário cadastrado com sucesso! \n");
+
+            } else if (opcaoMenuSelecionada == 2) {
+
+                System.out.println("------Zuppers Cadastrados------");
+
+                //Exibir lista Funcionarios
+                for (String impressaLista : listaFuncionarios.keySet()) {
+                    System.out.println("||Funcionario: " + listaFuncionarios.get(impressaLista) + " ||CPF: " + impressaLista);
+                }
+                System.out.print("\n");
+
+            } else if (opcaoMenuSelecionada == 3) {
+
+                //Sair
+                exibirMenu = false;
+                System.out.println("Até a proxima!");
+
+            } else {
+
+                System.out.println("Opção selecioanda inexistente. Selecione novamente! \n ");
+
+            }
+
         }
 
     }
+
 }
